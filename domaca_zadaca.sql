e7b60c0849c925b5ba71576796c44b7da65886f6
Alen Boric <boricalen@live.com>
2022-05-05 22:14:13 +0200

    Update domaca_zadaca.sql

--- domaca_zadaca.sql
+++ domaca_zadaca.sql
@@ -1,6 +1,24 @@
  # C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\domaca_zadaca.sql
 
-drop database if exists zadacajp26;
-create database zadacajp26;
-use zadacajp26;
+drop database if exists frizerski_salon;
+create database frizerski_salon;
+use frizerski_salon;
 
+create table djelatnica(
+    oib varchar(50),
+    ime_prezime varchar(50),
+    godina_rodenja varchar(50)
+);
+
+create table korisnici(
+    ime_prezime varchar(50),
+    broj_telefona varchar(50),
+    broj_korisnika varchar(50),
+    popust varchar(50)
+);
+
+create table usluga(
+    vrsta_sisanja varchar(50),
+    bojanje varchar(50),
+    frizura varchar(50)
+);
