47036703cf7684879d86e4a9f69e143f79a41b65
Alen Boric <boricalen@live.com>
2022-05-05 20:57:51 +0200

    Update domaca_zadaca.sql

--- domaca_zadaca.sql
+++ domaca_zadaca.sql
@@ -1,1 +1,6 @@
  # C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\domaca_zadaca.sql
+
+drop database if exists zadacajp26;
+create database zadacajp26;
+use zadacajp26;
+
