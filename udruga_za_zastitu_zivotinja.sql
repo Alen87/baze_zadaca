429743f498c57e6c2b89ce2f6e652c8b005f1b34
Alen Boric <boricalen@live.com>
2022-05-06 20:17:38 +0200

    Update udruga_za_zastitu_zivotinja.sql

--- udruga_za_zastitu_zivotinja.sql
+++ udruga_za_zastitu_zivotinja.sql
@@ -1,1 +1,18 @@
-#
+# C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\udruga_za_zastitu_zivotinja.sql
+
+drop database if exists udruga_za_zastitu_zivotinja;
+create database udruga_za_zastitu_zivotinja;
+use udruga_za_zastitu_zivotinja;
+
+create table osobe(
+    oib varchar(50),
+    ime_prezime varchar(50),
+    popis_sticenika varchar(50)
+);
+
+create table sticenici(
+    vrsta_sticenika varchar(50),
+    godina_starosti varchar(50),
+    podrijetlo varchar(50)
+);
+
