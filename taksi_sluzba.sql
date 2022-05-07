dfa22a6bba274e97b136851e43ceffff27049795
Alen Boric <boricalen@live.com>
2022-05-07 14:54:44 +0200

    Update taksi_sluzba.sql

--- taksi_sluzba.sql
+++ taksi_sluzba.sql
@@ -8,14 +8,27 @@ create table vozila(
     marka_vozila varchar(50),
     registracija varchar(50),
     gorivo_datum varchar(50),
-    registracija_datum varchar(50)
+    registracija_datum varchar(50),
+    vozac varchar(50)
 );
 
 create table vozac(
  ime_prezime varchar(50),
  oib varchar(50),
  datum_lijecnicki varchar(50),
- mjesto_prebivalista varchar(50)
+ mjesto_prebivalista varchar(50),
+ putnici varchar(50)
 );
 
-create table putnici();
+create table putnici(
+    ime_prezime varchar(50),
+    relacija varchar(50),
+    predeni_km varchar(50),
+    voznja varchar(50)
+);
+
+create table voznja(
+    nagradna_voznja varchar(50),
+    popust varchar(50),
+    trajanje varchar(50)
+);
