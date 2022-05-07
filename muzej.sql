947567f23c9693facc522b1b82aaab4d26581f4e
Alen Boric <boricalen@live.com>
2022-05-07 13:47:44 +0200

    Update muzej.sql

--- muzej.sql
+++ muzej.sql
@@ -1,1 +1,33 @@
-# C:\xampp\mysql\bin>mysql -uroot < 
+# C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\muzej.sql
+
+drop database if exists muzej;
+create database muzej;
+use muzej;
+
+create table izlozba(
+ br_izlozbe varchar(50),
+ ime_muzeja varchar(50),
+ ime_autora varchar(50),
+ datum_otvaranja varchar(50)
+);
+
+create table djelo(
+    tema varchar(50),
+    identifikacija varchar(50),
+    autor varchar(50)
+);
+
+create table kustos(
+    ime_prezime varchar(50),
+    oib varchar(50),
+    datum_rodenja varchar(50)
+);
+
+create table sponzor(
+    ime varchar(50),
+    adresa varchar(50),
+    uplata varchar(50)
+);
+
+
+
