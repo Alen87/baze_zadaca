008f50e54425c6c92599564083dfa54903026249
Alen Boric <boricalen@live.com>
2022-05-07 13:26:31 +0200

    Update djecji_vrtic.sql

--- djecji_vrtic.sql
+++ djecji_vrtic.sql
@@ -1,2 +1,28 @@
 # C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\djecji_vrtic.sql
 
+drop database if exists djecji_vrtic;
+create database djecji_vrtic;
+use djecji_vrtic;
+
+create table odgojna_skupina(
+    oznaka_skupine varchar(50),
+    broj_djece varchar(50),
+    dob_djece varchar(50)
+);
+
+create table djeca(
+    ime_prezime varchar(50),
+    datum_rodenja varchar(50),
+    adresa varchar(50)
+);
+
+create table odgajateljica(
+    ime_prezime varchar(50),
+    oib varchar(50),
+    adresa varchar(50),
+    strucna_sprema varchar(50)
+);
+
+
+
+
