34fb57a7a5fbb6e329202dec9eb733b09ff7e9c2
Alen Boric <boricalen@live.com>
2022-05-08 21:38:25 +0200

    Create postolar.sql

---
+++ postolar.sql
@@ -0,0 +1,10 @@
+# C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\postolar.sql
+
+drop database if exists postolar;
+create database postolar;
+use postolar;
+
+create table obuca(
+    materijal varchar(50),
+    vrsta 
+);
