9ab4bafebd9c5e08ebfaf3ba63e45d24a4b3e312
Alen Boric <boricalen@live.com>
2022-05-08 21:46:25 +0200

    Update postolar.sql

--- postolar.sql
+++ postolar.sql
@@ -6,5 +6,26 @@ use postolar;
 
 create table obuca(
     materijal varchar(50),
-    vrsta 
+    vrsta_obuce varchar(50),
+    broj_komada varchar(50)
+);
+
+create table korisnik(
+    ime_prezime varchar(50),
+    datum_zaprimanja varchar(50),
+    datum_popravka varchar(50)
+    
+);
+
+create table popravak(
+    popravak varchar(50),
+    utroseni_materijal varchar(50),
+    cijena_materijala varchar(50)
+);
+
+create table segrt(
+    ime_prezime varchar(50),
+    oib varchar(50),
+    radni_odnos varchar(50),
+    radna_odjeca varchar(50)
 );
