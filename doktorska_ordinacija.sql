e6f899a3d5d5cf683d4bfe3327188c66c08a6ee0
Alen Boric <boricalen@live.com>
2022-05-09 14:12:08 +0200

    Create doktorska_ordinacija.sql

---
+++ doktorska_ordinacija.sql
@@ -0,0 +1,26 @@
+# C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\doktorska_ordinacija.sql
+
+drop database if exists doktorska_ordinacija;
+create database doktorska_ordinacija;
+use doktorska_ordinacija;
+
+
+create table doktor (
+ ime_prezime varchar(50),
+ oib varchar(50),
+ zavrseno_zanimanje varchar(50)
+);
+
+create table pacijenti(
+    ime_prezime varchar(50),
+    br_kartona varchar(50),
+    oib varchar(50),
+    dopunsko varchar(50),
+    bolest varchar(50)
+);
+
+create table medicinske_sestre(
+    ime_prezime varchar(50),
+    oib varchar(50),
+    dezurstvo varchar(50),
+);
