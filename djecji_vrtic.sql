dac380cab838696f8981cc29abb52a5ed917b2c8
Alen Boric <boricalen@live.com>
2022-05-10 21:35:41 +0200

    Update djecji_vrtic.sql
    Unos podataka

--- djecji_vrtic.sql
+++ djecji_vrtic.sql
@@ -21,8 +21,8 @@ create table djeca(
 create table odgajateljica(
     sifra int not null primary key auto_increment,
     ime_prezime varchar(50) not null,
-    oib char(11),
-    adresa varchar(50),
+    oib char(11) not null,
+    adresa varchar(50) not null,
     strucna_sprema int
 );
 
@@ -38,3 +38,15 @@ create table strucna_sprema(
 alter table odgojna_skupina add foreign key (djeca) references djeca(sifra);
 alter table odgojna_skupina add foreign key (odgajateljica) references odgajateljica(sifra);
 alter table odgajateljica add foreign key (strucna_sprema) references strucna_sprema(sifra);
+
+# unos podataka 
+
+insert into odgajateljica (ime_prezime,oib,adresa)
+values ('Mirjana Horvat', '01234567898', 'Republika 12, Petrijevci');
+
+insert into djeca (ime_prezime,datum_rodenja,adresa)
+values ('Marko Klak','2019-05-21','Trpimirova 15, Osijek');
+
+
+
+
