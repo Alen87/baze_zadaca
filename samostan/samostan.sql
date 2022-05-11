b78a013be4eabf489e4fc62a6b1377bac110cc9c
Alen Boric <boricalen@live.com>
2022-05-11 17:14:22 +0200

    Update samostan.sql

--- samostan/samostan.sql
+++ samostan/samostan.sql
@@ -1,25 +1,43 @@
-# C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\samostan.sql
+# C:\xampp\mysql\bin>mysql -uroot --default_character_set=utf8 < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\samostan\samostan.sql
 
 drop database if exists samostan;
 create database samostan;
 use samostan;
 
-create table svecenici(
-    ime_prezime varchar(50),
-    oib varchar(50),
-    adresa varchar(50),
-    velicina_odore varchar(50)
+create table svecenik(
+    sifra int not null primary key auto_increment,
+    ime_prezime varchar(30) not null,
+    oib char(11) not null,
+    adresa varchar(30),
+    velicina_odore varchar(5),
+    posao int,
+    nadredeni_svecenik int
 );
 
-create table poslovi(
-    mise varchar(50),
-    ispovijedi varchar(50),
-    svecenje_kuca varchar(50)
+create table posao(
+    sifra int not null primary key auto_increment,
+    mise varchar(30),
+    ispovijedi varchar(30),
+    svecenje_kuca varchar(30),
+    svecenik int
 );
 
 create table nadredeni_svecenik(
-    ime_prezime varchar(50),
-    oib varchar(50),
+    sifra int not null primary key auto_increment,
+    ime_prezime varchar(30) not null,
+    oib char(11) not null,
     mjesto_rodenja varchar(50),
     velicina_odore varchar(50)
 );
+
+# definiranje vanjskih kljuceva
+alter table svecenik add foreign key (posao) references posao (sifra);
+alter table svecenik add foreign key (nadredeni_svecenik) references nadredeni_svecenik (sifra);
+alter table posao add foreign key (svecenik) references svecenik (sifra);
+
+# unos podataka
+insert into nadredeni_svecenik (ime_prezime,oib)
+values ('Marko Horvat','28594786094');
+
+insert into svecenik (ime_prezime,oib)
+values ('Ivan Ivić','75894893762');
