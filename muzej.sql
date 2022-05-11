74ba919d3398b9599418f94611c50bfec8643c28
Alen Boric <boricalen@live.com>
2022-05-11 13:23:03 +0200

    Update muzej.sql
    tipovi  podataka , unos  kljuceva  , unos  vrijednosti

--- muzej.sql
+++ muzej.sql
@@ -1,33 +1,50 @@
-# C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\muzej.sql
+# C:\xampp\mysql\bin>mysql -uroot --default_character_set=utf8 < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\muzej.sql
 
 drop database if exists muzej;
 create database muzej;
 use muzej;
 
 create table izlozba(
- br_izlozbe varchar(50),
- ime_muzeja varchar(50),
- ime_autora varchar(50),
- datum_otvaranja varchar(50)
+ sifra int not null primary key auto_increment, 
+ naziv_muzeja varchar(20) not null,
+ tema_izlozbe varchar(30) not null,
+ datum_otvaranja datetime not null,
+ sponzor int,
+ djelo int 
 );
 
 create table djelo(
-    tema varchar(50),
-    identifikacija varchar(50),
-    autor varchar(50)
+    sifra int not null primary key auto_increment,
+    naziv varchar(30) not null,
+    autor varchar(30) not null,
+    djelo_nastalo varchar(30)
 );
 
 create table kustos(
-    ime_prezime varchar(50),
-    oib varchar(50),
-    datum_rodenja varchar(50)
+    sifra int not null primary key auto_increment,
+    izlozba int,
+    ime_prezime varchar(30) not null,
+    oib char(11) not null,
+    datum_rodenja datetime
 );
 
 create table sponzor(
-    ime varchar(50),
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
     adresa varchar(50),
-    uplata varchar(50)
+    uplata decimal(18,3) not null
 );
 
+# definiranje vanjskih  kljuceva
+alter table izlozba add foreign key (sponzor) references sponzor(sifra);
+alter table izlozba add foreign key (djelo) references djelo(sifra);
 
+alter table kustos add foreign key (izlozba) references izlozba(sifra);
+
+# unos podataka
+insert into izlozba (naziv_muzeja,tema_izlozbe,datum_otvaranja)
+values ('Mimara','Ispod površine','2022-04-25');
+
+insert into kustos (ime_prezime,oib)
+values ('Marko Lopac','45637829074');
 
