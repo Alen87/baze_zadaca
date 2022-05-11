f15c7d8f55c55bc18f42f350c6a4a11e7e919483
Alen Boric <boricalen@live.com>
2022-05-11 11:25:27 +0200

    Update frizerski_salon.sql
    unos tipova  podataka

--- frizerski_salon.sql
+++ frizerski_salon.sql
@@ -1,24 +1,29 @@
- # C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\frizerski_salon.sql
+ # C:\xampp\mysql\bin>mysql -uroot --default_character_set=utf8 < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\frizerski_salon.sql
 
 drop database if exists frizerski_salon;
 create database frizerski_salon;
 use frizerski_salon;
 
 create table djelatnica(
-    oib varchar(50),
-    ime_prezime varchar(50),
-    godina_rodenja varchar(50)
+    sifra int not null primary key auto_increment,
+    oib char(11)not null,
+    ime_prezime varchar(20) not null,
+    godina_rodenja datetime,
+    korisnik int not null
 );
 
-create table korisnici(
-    ime_prezime varchar(50),
-    broj_telefona varchar(50),
-    broj_korisnika varchar(50),
-    popust varchar(50)
+create table korisnik(
+    sifra int not null primary key auto_increment,
+    ime_prezime varchar(20) not null,
+    broj_telefona varchar(20),
+    narucen datetime not null,
+    popust varchar(50),
+    usluga int not null
 );
 
 create table usluga(
-    vrsta_sisanja varchar(50),
-    bojanje varchar(50),
-    frizura varchar(50)
+    vrsta_sisanja varchar(30),
+    bojanje varchar(30),
+    frizura varchar(30),
+    cijena decimal(18,2)
 );
