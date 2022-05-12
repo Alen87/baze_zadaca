beb645c55fcc1833d8ad94cf5d837ad36f43ceec
Alen Boric <boricalen@live.com>
2022-05-12 16:37:43 +0200

    kljucevi,unos podataka

--- udruga_za_zastitu_zivotinja.sql
+++ udruga_za_zastitu_zivotinja.sql
@@ -1,24 +1,40 @@
-# C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\udruga_za_zastitu_zivotinja.sql
+# C:\xampp\mysql\bin>mysql -uroot --default_character_set=utf8 < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\udruga_za_zastitu_zivotinja.sql
 
 drop database if exists udruga_za_zastitu_zivotinja;
 create database udruga_za_zastitu_zivotinja;
 use udruga_za_zastitu_zivotinja;
 
-create table osobe(
-    oib varchar(50),
-    ime_prezime varchar(50),
-    popis_sticenika varchar(50)
+create table osoba(
+    sifra int not null primary key auto_increment,
+    oib char(11) not null,
+    ime_prezime varchar(30) not null,
+    adresa varchar(50),
+    broj_telefona varchar(30),
+    sticenik int 
 );
 
-create table sticenici(
-    vrsta_sticenika varchar(50),
-    godina_starosti varchar(50),
+create table sticenik(
+    sifra int not null primary key auto_increment,
+    vrsta  varchar(30) not null,
+    godina_starosti int not null,
     podrijetlo varchar(50),
-    br_prostora varchar(50)
+    prostor int
 );
 
 create table prostor(
-vrsta_prostora varchar(50),
-godina_izgradnje varchar(50),
-velicina_prostora varchar(50)
+sifra int not null primary key auto_increment,
+vrsta_prostora varchar(30) not null,
+oznaka varchar(10) not null,
+godina_izgradnje int,
+velicina_m2 int
 );
+
+alter table osoba add foreign key (sticenik) references sticenik(sifra);
+alter table sticenik add foreign key (prostor) references prostor(sifra);
+
+insert into sticenik (vrsta, godina_starosti)
+values ('Majmun', 10);
+
+insert into prostor (vrsta_prostora,oznaka)
+values ('Željezni kavez', 'M-513');
+
