484610890d237125204ef941c09efa4063915f71
Alen Boric <boricalen@live.com>
2022-05-12 23:05:20 +0200

    unos tipova  podataka

--- urar_silvija.sql
+++ urar_silvija.sql
@@ -1,31 +1,44 @@
-# C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\urar_silvija.sql
+# C:\xampp\mysql\bin>mysql -uroot --default_character_set=utf8 < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\urar_silvija.sql
 
 drop database if exists urar_silvija;
 create database urar_silvija;
 use urar_silvija;
 
-create table satovi(
-    vrsta_sata varchar(50),
-    serijski_broj varchar(50),
-    marka varchar(50),
-    datum_popravka varchar(50)
+create table urar(
+    sifra int not null primary key auto_increment,
+    ime_prezime varchar(30) not null,
+    oib char(11) not null,
+    adresa varchar(50),
+    sat int
+);
+
+create table sat(
+    sifra int not null primary key auto_increment,
+    vrsta_sata varchar(30) not null,
+    serijski_broj varchar(20) not null,
+    marka varchar(30) not null,
+    datum_popravka datetime,
+    popravak int
 );
 
 create table korisnik(
-    ime_prezime varchar(50),
-    satovi varchar(50),
-    oib varchar(50)
+    sifra int not null primary key auto_increment,
+    ime_prezime varchar(30) not null,
+    oib char(11) not null,
+    sat int
 );
 
 create table popravak(
-    vrsta_popravka varchar(50),
-    cijena_popravka varchar(50),
+    sifra int not null primary key auto_increment,
+    vrsta_popravka varchar(50) not null,
+    cijena_popravka decimal(18,2) not null,
     utroseni_materijal varchar(50)
 );
 
 create table segrt(
-    ime_prezime varchar(50),
-    oib varchar(50),
+    sifra int not null primary key auto_increment,
+    ime_prezime varchar(30) not null,
+    oib char(11) not null,
     radni_odnos varchar(50),
-    dnevnik_rada varchar(50)
+    popravci int
 );
