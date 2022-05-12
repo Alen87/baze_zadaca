f9ce5c3b2c766d1d3615c7b86ae4cd592b545f54
Alen Boric <boricalen@live.com>
2022-05-12 13:16:53 +0200

    unos tipova  podataka

--- taksi_sluzba.sql
+++ taksi_sluzba.sql
@@ -1,34 +1,43 @@
-# C:\xampp\mysql\bin>mysql -uroot < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\taksi_sluzba.sql
+# C:\xampp\mysql\bin>mysql -uroot --default_character_set=utf8 < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\taksi_sluzba.sql
 
 drop database if exists taksi_sluzba;
 create database taksi_sluzba;
 use taksi_sluzba;
 
 create table vozila(
-    marka_vozila varchar(50),
-    registracija varchar(50),
-    gorivo_datum varchar(50),
-    registracija_datum varchar(50),
-    vozac varchar(50)
+    sifra int not null primary key auto_increment,
+    naziv_vozila varchar(30) not null,
+    registracija varchar(30) not null,
+    registracija_istice datetime,
+    vozac int
 );
 
 create table vozac(
- ime_prezime varchar(50),
- oib varchar(50),
- datum_lijecnicki varchar(50),
- mjesto_prebivalista varchar(50),
- putnici varchar(50)
+  sifra int not null primary key auto_increment,
+  ime_prezime varchar(30) not null,
+  oib char(11) not null,
+  datum_lijecnicki datetime,
+  mjesto_prebivalista varchar(30)
 );
 
-create table putnici(
-    ime_prezime varchar(50),
-    relacija varchar(50),
-    predeni_km varchar(50),
-    voznja varchar(50)
+create table putnik(
+    sifra int not null primary key auto_increment,
+    ime_prezime varchar(30) not null,
+    relacija varchar(50) not null,
+    predeni_km varchar(50)
 );
 
 create table voznja(
-    nagradna_voznja varchar(50),
+    sifra int not null primary key auto_increment,
+    nagradna_voznja boolean,
     popust varchar(50),
-    trajanje varchar(50)
+    trajanje int,
+    cijena decimal(18,2) not null
 );
+
+create table putnik_voznja(
+    sifra int not null primary key auto_increment,
+    putnik int not null,
+    voznja int not null
+);
+
