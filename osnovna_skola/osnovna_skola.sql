35fbc2695a75c2f7604d3329b6d1453d93686ae4
Alen Boric <boricalen@live.com>
2022-05-13 23:42:38 +0200

    lokacija

--- osnovna_skola/osnovna_skola.sql
+++ osnovna_skola/osnovna_skola.sql
@@ -1,4 +1,4 @@
-# C:\xampp\mysql\bin>mysql -uroot --default_character_set=utf8 < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\osnovna_skola.sql
+# C:\xampp\mysql\bin>mysql -uroot --default_character_set=utf8 < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\osnovna_skola\osnovna_skola.sql
 
 drop database if exists osnovna_skola;
 create database osnovna_skola;
