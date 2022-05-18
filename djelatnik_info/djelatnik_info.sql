239afc4ad576a34eac225d199196f54a71bee8e1
Alen Boric <boricalen@live.com>
2022-05-18 17:44:03 +0200

    izmjena  kljuceva

--- djelatnik_info/djelatnik_info.sql
+++ djelatnik_info/djelatnik_info.sql
@@ -14,19 +14,18 @@ create table djelatnik(
   pocetak_rada datetime,
   loyality_kartica char(13) not null,
   radna_odjeca_obuca varchar(50),
-  sanitarna_iskaznica int not null,
   ugovor varchar(30),
   zavrseno_zanimanje varchar(50)
 );
 
 create table mobitel(
     sifra int not null primary key auto_increment,
+    djelatnik int,
     naziv varchar(40) not null,
     broj varchar(50) not null,
     datum_zaprimanja datetime not null,
     pravo_na_slijedeci datetime,
-    serijski_broj varchar(50),
-    djelatnik int
+    serijski_broj varchar(50)
     );
 
 create table edukacija(
@@ -39,6 +38,7 @@ create table edukacija(
 
 create table sanitarna_iskaznica(
     sifra int not null primary key auto_increment,  
+    djelatnik int not null,
     broj_iskaznice varchar(30),
     datum_obavljenog_pregleda datetime,
     vrijedi_do datetime,
@@ -53,7 +53,7 @@ create table sanitarna_iskaznica(
 
      #definiranje vanjskih  kljuceva
 
-    alter table djelatnik add foreign key (sanitarna_iskaznica) references sanitarna_iskaznica(sifra);
+    alter table sanitarna_iskaznica add foreign key (djelatnik) references djelatnik(sifra);
     alter table mobitel add foreign key (djelatnik) references djelatnik(sifra);
     alter table djelatnik_edukacija add foreign key (djelatnik) references djelatnik(sifra);
     alter table djelatnik_edukacija add foreign key (edukacija) references edukacija(sifra);
