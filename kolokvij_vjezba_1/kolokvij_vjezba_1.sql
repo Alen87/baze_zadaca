456916de5235b6274f767ebc333985889a39b4d8
Alen Boric <boricalen@live.com>
2022-05-26 12:35:28 +0200

    5. zadatak

--- kolokvij_vjezba_1/kolokvij_vjezba_1.sql
+++ kolokvij_vjezba_1/kolokvij_vjezba_1.sql
@@ -1,4 +1,5 @@
 # C:\xampp\mysql\bin>mysql -uroot --default_character_set=utf8 < C:\Users\Alen\Documents\GitHub\Alen87\baze_zadaca\kolokvij_vjezba_1\kolokvij_vjezba_1.sql
+# 0. Kreirajte tablice (16) i veze između tablica. (6)
 
 drop database if exists kolokvij_vjezba_1;
 create database kolokvij_vjezba_1;
@@ -90,6 +91,7 @@ create table sestra_svekar(
  alter table sestra_svekar add foreign key (svekar) references svekar(sifra);
  alter table cura add foreign key (punac) references punac(sifra);
 
+ # 1. U tablice muskarac, zena i sestra_svekar unesite po 3 retka. (7)
 
  insert into muskarac(sifra,bojaociju,hlace,modelnaocala,maraka)
  values (null,'zelene','plave','okrugle',25.45657845),
@@ -120,4 +122,37 @@ values (null,1,3),
        (null,3,1);
 
 
-       
+# 2. U tablici cura postavite svim zapisima kolonu gustoca na vrijednost 15,77. (4)
+
+update cura set gustoca=15.77;
+
+#3. U tablici mladic obrišite sve zapise čija je vrijednost kolone kuna veće od 15,78. (4)
+   
+select * from mladic;
+insert into mladic(sifra,suknja,kuna,drugiputa,asocijalno,ekstroventno,dukserica,muskarac)
+values (null,'kratka',15.34,null,null,1,'zelena',null),
+       (null,'zelena',25.45,null,null,1,'zelena',null),
+       (null,'plava',12.54,null,null,1,'zelena',null);
+
+delete from mladic where kuna>15.78;
+
+#4. Izlistajte kratkamajica iz tablice zena uz uvjet da vrijednost kolone hlace sadrže slova ana. (6)
+
+select kratkamajica from zena 
+where hlace like '%ana%';
+
+#5. Prikažite dukserica iz tablice svekar, asocijalno iz tablice mladic te 
+#hlace iz tablice muskarac uz uvjet da su vrijednosti kolone hlace iz 
+#tablice zena počinju slovom a te da su vrijednosti kolone haljina iz 
+#tablice sestra sadrže niz znakova ba. Podatke posložite po hlace iz 
+#tablice muskarac silazno. (10)
+
+select a.dukserica,f.asocijalno,e.hlace 
+from svekar a
+inner join sestra_svekar b on b.svekar = a.sifra 
+inner join sestra c on c.sifra = b.sestra 
+inner join zena d on d.sestra = c.sifra 
+inner join muskarac e on e.zena =d.sifra 
+inner join mladic f on f.muskarac = e.sifra 
+where d.hlace like 'a%' and c.haljina like '%ba%'
+order by e.hlace desc;
