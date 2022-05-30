1b66dcf6eec9c5b64fd6c84f6e8e59f894d8fe52
Alen Boric <boricalen@live.com>
2022-05-30 11:14:07 +0200

    svi_zadaci,rijesenja

--- kolokvij_vjezbe/kolokvij_vjezba_5/kolokvij_vjezba_5.sql
+++ kolokvij_vjezbe/kolokvij_vjezba_5/kolokvij_vjezba_5.sql
@@ -81,3 +81,82 @@ alter table punac add foreign key (svekar) references svekar(sifra);
 alter table punica add foreign key (punac) references punac(sifra);
 alter table ostavljena add foreign key (punica) references punica(sifra);
 alter table mladic add foreign key (zarucnik) references zarucnik(sifra);
+
+# 1. U tablice punica, punac i svekar_cura unesite po 3 retka. (7)
+
+insert into cura(sifra,carape,maraka,asocijalno,vesta)
+values(null,'bijele',23.45,null,'vunena'),
+      (null,'crvene',12.45,null,'vunena'),
+      (null,'crne',45.45,null,'vunena');
+      
+     
+ insert into svekar(sifra,bojakose,majica,carape,haljina,narukvica,eura)
+ values(null,null,null,'crne',null,null,34.12),
+       (null,null,null,'bijele',null,null,34.67),
+       (null,null,null,'crvene',null,null,32.12);
+     
+insert into svekar_cura (sifra,svekar,cura)
+values(null,1,2),
+      (null,2,3),
+      (null,2,1);
+
+insert into punac(sifra,dukserica,prviputa,majica,svekar)
+values(null,null,'2022-05-30',null,2),
+      (null,null,'2022-03-30',null,1),
+      (null,null,'2022-04-30',null,3);
+
+insert into punica(sifra,hlace,nausnica,ogrlica,vesta,modelnaocala,treciputa,punac)
+values(null,'crne',4,null,'žuta','sunčane','2022-05-30',1),
+      (null,'crne',5,null,'crna','sunčane','2022-05-30',2),
+      (null,'crne',6,null,'žuta','sunčane','2022-05-30',3);
+
+#  2. U tablici mladic postavite svim zapisima kolonu haljina na vrijednost Osijek. (4)
+
+insert into mladic(sifra,kratkamajica,haljina,asocijalno,zarucnik)
+values(null,'bijela','zelena',null,null),
+      (null,'žuta','crvena',null,null),
+      (null,'crna','bijela',null,null);
+      
+  update mladic set haljina = 'Osijek';   
+             
+# 3. U tablici ostavljena obrišite sve zapise čija je vrijednost kolone ogrlica jednako 17. (4)
+
+insert into ostavljena(sifra,majica,ogrlica,carape,stilfrizura,punica)
+values(null,null,4,null,null,1),
+      (null,null,2,null,null,2),
+      (null,null,17,null,null,3);
+
+
+delete from ostavljena where ogrlica = 17;
+     
+# 4. Izlistajte majica iz tablice punac uz uvjet da vrijednost kolone prviputa nepoznate. (6)
+
+select majica from punac 
+where prviputa is null;
+
+# 5. Prikažite asocijalno iz tablice cura, stilfrizura iz tablice ostavljena te 
+# nausnica iz tablice punica uz uvjet da su vrijednosti kolone prviputa iz 
+# tablice punac poznate te da su vrijednosti kolone majica iz tablice 
+# svekar sadrže niz znakova ba. Podatke posložite po nausnica iz tablice 
+# punica silazno. (10)
+
+select a.asocijalno ,f.stilfrizura ,e.nausnica 
+from cura a
+inner join svekar_cura b on b.cura = a.sifra 
+inner join svekar c on b.svekar = c.sifra 
+inner join punac d on d.svekar = c.sifra 
+inner join punica e on e.punac = d.sifra 
+inner join ostavljena f on f.punica = e.sifra
+where d.prviputa is not null and c.majica like '%ba%'
+order by e.nausnica desc;
+
+
+# 6. Prikažite kolone majica i carape iz tablice svekar čiji se primarni ključ ne nalaze u tablici svekar_cura. (5)
+
+select a.majica ,a.carape 
+from svekar a
+left join svekar_cura b on b.svekar = a.sifra 
+where b.sifra is null;
+
+
+
